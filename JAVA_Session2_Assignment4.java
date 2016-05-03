0db0c8d3f089883d9e8bc6169718dded4cabd98e
Deepakp404 <deepakp404@gmail.com>
2016-05-03 13:20:50 +0530

    No of days in a given month

---
+++ JAVA_Session2_Assignment4.java
@@ -0,0 +1,64 @@
+
+public class JAVA_Session2_Assignment4 
+{
+	public static void main(String[] args) 
+	{
+		int month = 3;
+		
+		switch(month){
+		
+		case 1: 
+			System.out.println("Entered Month is Jan No of days is 31");
+			break;
+			
+		case 2: 
+			System.out.println("Entered Month is Feb No of days is 28");
+			break;
+			
+		case 3: 
+			System.out.println("Entered Month is Mar No of days is 31");
+			break;
+			
+		case 4: 
+			System.out.println("Entered Month is Apr No of days is 30");
+			break;
+			
+		case 5: 
+			System.out.println("Entered Month is May No of days is 31");
+			break;
+			
+		case 6: 
+			System.out.println("Entered Month is June No of days is 30");
+			break;
+			
+		case 7: 
+			System.out.println("Entered Month is July No of days is 31");
+			break;
+			
+		case 8: 
+			System.out.println("Entered Month is Aug No of days is 31");
+			break;
+			
+		case 9: 
+			System.out.println("Entered Month is Sep No of days is 30");
+			break;
+			
+		case 10: 
+			System.out.println("Entered Month is Oct No of days is 31");
+			break;
+			
+		case 11: 
+			System.out.println("Entered Month is Nov No of days is 30");
+			break;
+			
+		case 12: 
+			System.out.println("Entered Month is Dec No of days is 31");
+			break;
+		
+		default :
+			System.out.println("Please enter month between 1 to 12");
+			break;
+		}
+	}
+
+}
